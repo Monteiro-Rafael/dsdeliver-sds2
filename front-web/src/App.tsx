1972922b1a42107cf20b2a7a2761ab7756292f9b
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-01-10 01:31:59 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
+import Home from './Home';
 import NavBar from './NavBar';
 
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
